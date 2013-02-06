2f43b323e1067eb7a7ac96a200790606f20fcfac
Gerardo Cotrina <gerardo.cotrina@gmail.com>
2013-02-06 00:34:28 -0500

    Administrador de torneos y locales

--- db/migrate/20130130062234_create_torneos.rb
+++ db/migrate/20130130062234_create_torneos.rb
@@ -5,7 +5,7 @@ class CreateTorneos < ActiveRecord::Migration
       t.integer :cant_part
       t.date :fecha
       t.datetime :hora
-      t.references :local
+      t.references :local.nombre
 
       t.timestamps
     end
