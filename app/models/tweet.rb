7e8bb2b3427f0c97d374a55de201a3876d99c1a3
Paul Cabrera Sanchez <paul.cabrera.sanchez@gmail.com>
2013-02-10 13:10:27 -0500

    Completando Comentarios de TWITTER en Torno
    Los Usuario pueden TWITTERAR comentarios desde la Aplicacion

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,11 +7,20 @@ class Tweet < ActiveRecord::Base
  #   config.oauth_token_secret = 'mYxB32QN51IflTkeRTBwYfryhVqRmv40Lez8Sis'
  # end
 
+#Twitter.configure do |config|
+#    config.consumer_key = 'VXZCK1w2BpERLGJPeLueig'
+#    config.consumer_secret = 'SD6RPNoSbBNsJSCfAB7eVIk7M2SgUodorQeRq4TI1E'
+#    config.oauth_token = '344334932-QveQmkQFEGd1Iw6HxdBYVxk8PCCDHgYDsP8wtBxs'
+#    config.oauth_token_secret = '5yaXJHltED07u0LGnCTDP3n5nPSPOntD6RryutKT6s'
+#  end
+
+# Datos para Direccion a mi Propia cuenta TWITTER
+#-------------------------------------------------
 Twitter.configure do |config|
-    config.consumer_key = 'VXZCK1w2BpERLGJPeLueig'
-    config.consumer_secret = 'SD6RPNoSbBNsJSCfAB7eVIk7M2SgUodorQeRq4TI1E'
-    config.oauth_token = '344334932-QveQmkQFEGd1Iw6HxdBYVxk8PCCDHgYDsP8wtBxs'
-    config.oauth_token_secret = '5yaXJHltED07u0LGnCTDP3n5nPSPOntD6RryutKT6s'
+    config.consumer_key = 'Go3cSXZ0TL2xUwLV3fTig'
+    config.consumer_secret = 'noSuggae6p8uqagO9ZYqKnkpNud4UIorXqdjR3hio'
+    config.oauth_token = '1143023234-5oeTjqbcsbdlGIIIsHm25dluhoz2oL4iy8Wg3ad'
+    config.oauth_token_secret = '1WJZCAKITbNpzkudXMfNqRheEtumOXd8thD53Djk'
   end
 
 
